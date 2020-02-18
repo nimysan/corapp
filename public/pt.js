0786916ebf235bea7e43de45ae97f18d33e705f5
Seann <yexw@btte.net>
2020-02-18 14:34:10 +0800

    add

---
+++ public/pt.js
@@ -0,0 +1,204 @@
+const themeColor = '#FF3366';
+const centerOfShenzhen = [116.397551, 39.906947];
+const map = new AMap.Map('ncovMap', {
+    //center: centerOfShenzhen,
+    zoom: 12,
+    viewMode: '2D',
+    lang: 'zh_cn'
+});
+var currentMarker = null;
+var oldMarkerGroup = null;
+var oldMarekrOverlay = null;
+const fullList = [];
+
+//远程读取数据
+$.get("/data", function(data) {
+    for (var i = 0; i < data.length; i++) {
+        fullList.push(data[i]);
+    }
+});
+
+function loadUnlucky(datafile) {
+    return; // instead of read data from remote 
+    $.get(datafile, function(data) {
+        // do something with your data
+        for (var i = 0; i < data.length; i++) {
+            var dist = data[i];
+            if (dist.pois) {
+                for (var p = 0; p < dist.pois.length; p++) {
+                    fullList.push(dist.pois[p]);
+                }
+            }
+        }
+    });
+}
+
+loadUnlucky("gd.json");
+loadUnlucky("ah.json");
+loadUnlucky("bj.json");
+loadUnlucky("fj.json");
+loadUnlucky("hb.json");
+loadUnlucky("hn.json");
+loadUnlucky("js.json");
+loadUnlucky("jx.json");
+loadUnlucky("sc.json");
+loadUnlucky("sd.json");
+//loadUnlucky("ah.json");
+
+
+
+
+/**
+ * 查看感染小区列表与给定的列表之间的距离，默认距离为3公里之内
+ */
+function inMiles(p1, miles, list) {
+    var meters = miles * 1000;
+    var unlucky = [];
+    for (var m in list) {
+        var dist = list[m];
+        var dis = AMap.GeometryUtil.distance(p1, dist.point.coordinates);
+        if (dis <= meters) {
+            //console.log("---- " + dis + " --- " + dist.name);
+            unlucky.push(dist);
+        } else {
+            //console.log("lucky " + dis + " --- " + dist.name);
+        }
+    }
+    return unlucky;
+}
+
+function paintUnlucks(map, tmp_data) {
+
+    if (oldMarekrOverlay) { map.remove(oldMarekrOverlay); }
+    if (oldMarkerGroup) { map.remove(oldMarkerGroup); }
+    const markers = [];
+    const overlays = [];
+    //var tmp_data = [];
+
+    tmp_data.forEach(d => {
+        let overlay;
+        if (d.geometry && d.geometry.type === 'Polygon') {
+            overlay = new AMap.Polygon({ path: d.geometry.coordinates });
+        } else if (d.geometry && d.geometry.type === 'LineString') {
+            overlay = new AMap.Polyline({ path: d.geometry.coordinates });
+        } else {
+            overlay = new AMap.Circle({
+                center: d.point.coordinates,
+                radius: 50
+            })
+        }
+        const marker = new AMap.Marker({
+            position: d.point.coordinates,
+            icon: new AMap.Icon({
+                size: new AMap.Size(8, 8),
+                image: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAABGdBTUEAALGPC/xhBQAAAA5JREFUGBljYBgFoBAAAAEIAAGUnFwzAAAAAElFTkSuQmCC',
+                imageSize: new AMap.Size(8, 8)
+            }),
+            offset: new AMap.Pixel(-4, -8),
+        });
+        const content = `<div class="poi-label" style="background-color:${themeColor}">${d.name}<i class="poi-label-arrow" style="border-color: ${themeColor};border-style: dashed;border-top-style: solid;border-left-color: transparent;border-right-color: transparent;"></i></div>`
+        marker.setLabel({
+            offset: new AMap.Pixel(0, 0),
+            content,
+            direction: 'top'
+        });
+
+        markers.push(marker);
+        overlays.push(overlay);
+    });
+
+    const overlayGroup = new AMap.OverlayGroup(overlays);
+    const markerGroup = new AMap.OverlayGroup(markers);
+
+    overlayGroup.setOptions({
+        borderWeight: 1,
+        strokeColor: themeColor,
+        lineJoin: 'round',
+        fillColor: themeColor,
+        fillOpacity: 0.4
+    });
+
+    map.add(overlayGroup);
+    map.add(markerGroup);
+    oldMarekrOverlay = overlayGroup;
+    oldMarkerGroup = markerGroup;
+
+    map.on('zoomend', function(e) {
+        if (map.getZoom() <= 11) {
+            markerGroup.hide();
+        } else {
+            markerGroup.show();
+        }
+    })
+}
+
+
+AMapUI.loadUI(['control/BasicControl', 'misc/PoiPicker'], function(BasicControl, PoiPicker) {
+
+    var poiPicker = new PoiPicker({
+        //city:'北京',
+        input: 'pickerInput'
+    });
+
+    //初始化poiPicker
+    poiPickerReady(poiPicker);
+});
+
+function poiPickerReady(poiPicker) {
+
+    window.poiPicker = poiPicker;
+
+    var marker = new AMap.Marker();
+
+    var infoWindow = new AMap.InfoWindow({
+        offset: new AMap.Pixel(0, -20)
+    });
+
+    //选取了某个POI
+    poiPicker.on('poiPicked', function(poiResult) {
+
+        var source = poiResult.source,
+            poi = poiResult.item,
+            info = {
+                source: source,
+                id: poi.id,
+                name: poi.name,
+                location: poi.location.toString(),
+                address: poi.address
+            };
+
+        marker.setMap(map);
+        //infoWindow.setMap(map);
+
+        var markerCurrent = new AMap.Marker({
+            position: poi.location, // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
+            title: poi.name,
+            //content: poi.name
+        });
+        marker.setPosition(poi.location);
+        //infoWindow.setPosition(poi.location);
+
+        //infoWindow.setContent('POI信息: <pre>' + JSON.stringify(info, null, 2) + '</pre>');
+        //infoWindow.open(map, marker.getPosition());
+        if (currentMarker) {
+            map.remove(currentMarker);
+        }
+        map.add(markerCurrent);
+        currentMarker = markerCurrent;
+
+        var unluckyNear = inMiles(poi.location, 3, fullList);
+        if (unluckyNear.length > 0) {
+
+            paintUnlucks(map, unluckyNear);
+        }
+        //console.log("0000 " + unluckyNear);
+
+        map.setFitView();
+        markerCurrent.setAnimation('AMAP_ANIMATION_BOUNCE');
+        //map.setCenter(marker.getPosition());
+    });
+
+    poiPicker.onCityReady(function() {
+        poiPicker.suggest('7天酒店');
+    });
+}
---
+++ server.js
@@ -0,0 +1,93 @@
+const Koa = require('koa');
+const app = new Koa();
+const path = require('path'); //系统路径模块
+const fs = require('fs'); //文件模块
+const Router = require('koa-router');
+
+const staticFiles = require('koa-static');
+// 指定 public目录为静态资源目录，用来存放 js css images 等
+app.use(staticFiles(path.resolve(__dirname, "./public")))
+
+let fullList = [];
+let cacheTime = null;
+
+
+
+// logger
+app.use(async (ctx, next) => {
+    await next();
+    const rt = ctx.response.get('X-Response-Time');
+    console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+let home = new Router();
+home.get('data/', async (ctx) => {
+    composeAllData();
+    ctx.body = fullList;
+});
+// 装载所有子路由
+let router = new Router()
+router.use('/', home.routes(), home.allowedMethods())
+//router.use('/page', page.routes(), page.allowedMethods())
+
+// 加载路由中间件
+app.use(router.routes()).use(router.allowedMethods())
+
+function composeAllData() {
+    let nowTime = (new Date()).getTime();
+    if (cacheTime && (nowTime - cacheTime) < 1000 * 60 * 60 * 12) {
+        return;
+    } //12个小时一次
+    cacheTime = nowTime;
+    fullList = [];
+    console.log("READ again");
+    var coronavirus = path.join(__dirname, 'coronavirus');
+    fs.readdir(coronavirus, function(err, files) {
+        files.forEach(function(filename) {
+            fs.stat(path.join(coronavirus, filename), function(err, stats) {
+                if (stats.isFile() && gettype(filename) == 'json') {
+                    if (filename != "GaoDeYiQin.json") {
+                        console.log("--- filename ---" + filename);
+                        fs.readFile(path.join(coronavirus, filename), "UTF-8", function(err, jsonData) {
+                            try {
+                                let tempData = JSON.parse(jsonData);
+                                for (var i = 0; i < tempData.length; i++) {
+                                    var dist = tempData[i];
+                                    if (dist.pois) {
+                                        for (var p = 0; p < dist.pois.length; p++) {
+                                            fullList.push(dist.pois[p]);
+                                        }
+                                    }
+                                }
+                            } catch (e) {
+                                console.log("exception --- filename ---" + filename);
+                            }
+
+
+                        });
+
+                    }
+
+                }
+            });
+        })
+    });
+    console.log("--- fullList--- seize:" + fullList);
+}
+
+//获取后缀名
+function gettype(url) {
+    var arr = url.split('.');
+    var len = arr.length;
+    return arr[len - 1];
+}
+
+app.listen(3000);
