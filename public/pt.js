d15c224c09ceda9e9ab3bef2e63d004779a26726
Seann <yexw@btte.net>
2020-02-18 17:50:39 +0800

    don't load data at first

--- public/pt.js
+++ public/pt.js
@@ -12,13 +12,14 @@ var oldMarekrOverlay = null;
 const fullList = [];
 
 //远程读取数据
-$.get("/data/update", function(data) {
-    /* 这个是结合本地计算使用的，如果使用inMiles方法，那么此处应该处理
+/*
+$.get("/data", function(data) {
+    这个是结合本地计算使用的，如果使用inMiles方法，那么开启此方法
     for (var i = 0; i < data.length; i++) {
         fullList.push(data[i]);
-    }*/
+    }
 });
-
+*/
 function loadUnlucky(datafile) {
     return; // instead of read data from remote 
     $.get(datafile, function(data) {
